56b53dbdb500c98ba93d24d10e61fb54eb7b89ed
Anu Khambete <anukhambete@gmail.com>
2018-06-27 17:58:50 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "My name is Princess Consuela Bananahammock!!"
     resp.finish
   end
 
 end
-
